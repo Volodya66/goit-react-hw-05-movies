884842f692716dedaf16c68f16e0d9ae59f07b14
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-11-07 17:02:37 +0200

    с

--- src/components/MovieDetailCard/MovieDetailCard.jsx
+++ src/components/MovieDetailCard/MovieDetailCard.jsx
@@ -15,11 +15,11 @@ return(
      <button type="button">Back</button>
      <div><img src={`https://image.tmdb.org/t/p/w500${cardDetails.poster_path}`} alt="" width='350' height='230'/></div>
     <div>   
-    <h1></h1>
+    {/* <h1></h1>
     <p></p>
     <h2></h2>
     <p></p>
-    <h2></h2>
+    <h2></h2> */}
     <p></p>
     </div>    
             </>
