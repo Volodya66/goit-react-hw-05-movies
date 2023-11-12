2f35c495e506002995693d2c22e3ff179a02e59c
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-11-12 13:45:08 +0000

    Deploying to gh-pages from @ Volodya66/goit-react-hw-05-movies@b7bbc66bc04aad5429e9e2506c8243dd032f3afc 🚀

---
+++ static/js/321.a41ea85a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[321],{321:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(920),o=r(184);function a(){return(0,o.jsx)(n.s5,{strokeColor:"grey",strokeWidth:"3",animationDuration:"0.75",width:"102",visible:!0})}}}]);
+//# sourceMappingURL=321.a41ea85a.chunk.js.map
